cadba00fd4e87e34ccd7cae951291dc58617ed35
Rydha Valenda <ryval.study@gmail.com>
2020-04-16 00:38:47 +0700

    Update 03 - Process title website for all page using {{TITLE}}, base from jinja - Inheritance (check file "index.html" and "about.html") - Create "settings.py" to debug configuration

--- web_app/app.py
+++ web_app/app.py
@@ -2,15 +2,20 @@ from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
+    # konfigurasi debug dibawah berfungsi flask mengambil perubahan2 dalam html
+    # app.config['DEBUG'] = True
+
+    # file config diambil dari file mode py
+    app.config.from_pyfile('settings.py')
 
     # "route" alamat url yang akan ditangani oleh app ini
     @app.route('/') #decorator(akan memastikan fungsi index akan bisa dipanggil oleh flask)
     def index():
-        return render_template('index.html')
+        return render_template('index.html', TITLE='Flask-01')
 
-    @app.route('/abput')
+    @app.route('/about')
     def about():
-        return 'About me'
+        return render_template('about.html', TITLE='Flask-01')
 
     return app
 
---
+++ web_app/settings.py
@@ -0,0 +1,2 @@
+# konfigurasi debug dibawah berfungsi flask mengambil perubahan2 dalam html
+DEBUG = True
